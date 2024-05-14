8d57434d36fe341311fbf1107327b3ba3ada5fa4
e-n-p <nickellam0@gmail.com>
2024-05-14 10:43:45 +0200

    quest 3

---
+++ src/Indy/LastCrusade.java
@@ -0,0 +1,15 @@
+package Indy;
+
+public class LastCrusade {
+    public static void main(String[] args) {
+        String title = "Indiana Jones and the Last Crusade";
+        boolean haveSeen = true;
+        int releaseYear = 1989;
+        float reviewRating = 8.2F;
+
+        System.out.println(
+                title + ", has been seen: " + haveSeen +
+                        " release year: " + releaseYear + ", rating: " + reviewRating
+        );
+    }
+}
